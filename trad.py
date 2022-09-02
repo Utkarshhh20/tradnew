a1b267341a426f846e66f6259c7a2355d2f2d877
Utkarshhh20 <88526325+Utkarshhh20@users.noreply.github.com>
2022-09-02 16:54:10 +0400

    Update trad.py

--- trad.py
+++ trad.py
@@ -37,6 +37,47 @@ from bollingerband import BOLLStrat
 from goldencrossover import goldencrossover
 symbol='AAPL'
 newscount=0
+def news_headlines(ticker):
+    url = finviz_url + ticker
+    req = Request(url=url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}) 
+    response = urlopen(req)    
+    # Read the contents of the file into 'html'
+    html = bs(response)
+    # Find 'news-table' in the Soup and load it into 'news_table'
+    news_table = html.find(id='news-table')
+    return news_table
+	
+# parse news into dataframe
+def parse_news(news_table):
+    parsed_news = []
+    
+    for x in news_table.findAll('tr'):
+        # read the text from each tr tag into text
+        # get text from a only
+        text = x.a.get_text() 
+        # splite text in the td tag into a list 
+        date_scrape = x.td.text.split()
+        # if the length of 'date_scrape' is 1, load 'time' as the only element
+
+        if len(date_scrape) == 1:
+            time = date_scrape[0]
+            
+        # else load 'date' as the 1st element and 'time' as the second    
+        else:
+            date = date_scrape[0]
+            time = date_scrape[1]
+        
+        # Append ticker, date, time and headline as a list to the 'parsed_news' list
+        parsed_news.append([date, time, text])        
+        # Set column names
+        columns = ['date', 'time', 'headline']
+        # Convert the parsed_news list into a DataFrame called 'parsed_and_scored_news'
+        parsed_news_df = pd.DataFrame(parsed_news, columns=columns)        
+        # Create a pandas datetime object from the strings in 'date' and 'time' column
+        parsed_news_df['datetime'] = pd.to_datetime(parsed_news_df['date'] + ' ' + parsed_news_df['time'])
+        
+    return parsed_news_df
+'''
 def get_news():
     try:
         # Find news table
@@ -74,3 +115,4 @@ for i in range(len(news)):
                         st.write('____________________')
                     if newscount==15:
                         break
+'''
